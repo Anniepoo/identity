198bf5321248e1d8768ae7d3fff78589ec88e5f4
Anne Ogborn <annie66us@yahoo.com>
2019-03-16 12:21:12 +0530

    release 0.2.2 - first full functionality release.

--- pack.pl
+++ pack.pl
@@ -2,12 +2,12 @@
 %
 name(identity).
 title('Registration, login, and role management').
-version('0.2.1').
+version('0.2.2').
 author('Anne Ogborn', 'anne@swi-prolog.org').
 maintainer('Anne Ogborn', 'anne@swi-prolog.org').
 packager('Anne Ogborn', 'anne@swi-prolog.org').
 home('https://github.com/Anniepoo/identity').
-download('http://github.com/Anniepoo/identity/archive/master.zip').
+download('http://github.com/Anniepoo/identity/archive/0.2.2.zip').
 provides(identity).
 
 
